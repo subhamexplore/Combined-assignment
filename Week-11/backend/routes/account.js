7880882479bedb44b01c2f87878f733a6193864c
subhamexplore <subhamparidamain@gmail.com>
2026-05-11 08:43:31 +0530

    done using mongoDB transaction

--- Week-11/backend/routes/account.js
+++ Week-11/backend/routes/account.js
@@ -1,35 +1,58 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const {auth} = require('../middleware')
+const { auth } = require("../middleware");
 const { userModel, accountModel } = require("../db");
 
 router.get("/balance", auth, async (req, res) => {
-    const userId = req.userId
-    const data = await accountModel.findOne({userId})
-    const balance = data.balance
-    res.json({
-        message: `Your balance is ${balance}.`
-    })
-})
+  const userId = req.userId;
+  const data = await accountModel.findOne({ userId });
+  const balance = data.balance;
+  res.json({
+    message: `Your balance is ${balance}.`,
+  });
+});
 
 router.post("/transfer", auth, async (req, res) => {
-    const userId = req.userId
-    const {username, pay} = req.body
-    const userExists = await userModel.findOne({username})
-    if(!userExists){
-        return res.status(411).json({message: "The user you want to pay does not exist."})
-    }
+  const userId = req.userId;
+  const { username, pay } = req.body;
+  const userExists = await userModel.findOne({ username });
+  if (!userExists) {
+    return res
+      .status(411)
+      .json({ message: "The user you want to pay does not exist." });
+  }
 
-    const dataFrom = await accountModel.findOne({userId})
-    const dataTo = await accountModel.findOne({userId: userExists._id})
-    let balanceFrom = dataFrom.balance - pay
-    let balanceTo = dataTo.balance + pay
-    
-    await accountModel.updateOne({userId: userExists._id}, {balance: balanceTo})
-    await accountModel.updateOne({userId}, {balance: balanceFrom})
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  const dataFrom = await accountModel.findOne({ userId: req.userId });
+  if (dataFrom.balance < pay || pay <= 0) {
+    await session.abortTransaction();
+    return res.status(400).json({ message: "Insufficient balance." });
+  }
+
+  try {
+    await accountModel.updateOne(
+      { userId: userExists._id },
+      { $inc: { balance: pay } },
+      { session },
+    );
+    await accountModel.updateOne(
+      { userId },
+      { $inc: { balance: -pay } },
+      { session },
+    );
+    await session.commitTransaction();
     res.json({
-        message: `${pay} was debited to ${username}.`
-    })
-})
+      message: "Transfer Successful.",
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    res.status(500).json({ message: "Transfer Failed." });
+  } finally {
+    session.endSession();
+  }
+});
 
 module.exports = router;
